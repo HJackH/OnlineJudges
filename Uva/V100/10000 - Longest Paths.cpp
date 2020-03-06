b76e2d710654d90ff84ce8821a763e4ff8c3d91a
HJackH <56967377+HJackH@users.noreply.github.com>
2020-03-06 22:12:01 +0800

    create uva category

---
+++ Uva/V100/10000 - Longest Paths.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 200;
+
+struct Edge {
+    int to;
+    int cost;
+
+    Edge(int _to, int _cost) {
+        to = _to;
+        cost = _cost;
+    }
+
+    bool operator < (Edge const &other) const {
+        return cost < other.cost;
+    }
+};
+
+int N, start, kase = 0;
+vector<Edge> G[MAXN + 10];
+vector<int> dis(MAXN + 10);
+
+void init() {
+    for (int i = 0; i <= N; i++) {
+        G[i].clear();
+        dis[i] = 0;
+    }
+}
+
+void djk() {
+    priority_queue<Edge, vector<Edge>> pq;
+    pq.push(Edge(start, 0));
+    dis[start] = 0;
+
+    while (!pq.empty()) {
+        Edge u = pq.top();
+        pq.pop();
+
+        if (u.cost != dis[u.to]) {
+            continue;
+        }
+
+        for (Edge i : G[u.to]) {
+            if (dis[i.to] < i.cost + u.cost) {
+                dis[i.to] = i.cost + u.cost;
+                pq.push(Edge(i.to, dis[i.to]));
+            }
+        }
+    }
+}
+    
+int main() {
+
+    while (cin >> N) {
+        if (N == 0) {
+            break;
+        }
+
+        init();
+
+        cin >> start;
+        int p, q;
+        while (cin >> p >> q) {
+            if (p == 0 && q == 0) {
+                break;
+            }
+
+            G[p].push_back(Edge(q, 1));
+        }
+
+        djk();
+
+        int anslen = 0, anspoint;
+        for (int i = 1; i <= N; i++) {
+            if (dis[i] > anslen) {
+                anslen = dis[i];
+                anspoint = i;
+            }
+        }
+
+        cout << "Case " << ++kase << ": The longest path from " << start
+            << " has length " << anslen << ", finishing at " << anspoint << "." << endl;
+        cout << endl;
+    }
+}
