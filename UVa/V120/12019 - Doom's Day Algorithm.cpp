0885f2462f5075cb785b94d8dee11410a513c90e
HJackH <fandy36@gmail.com>
2021-03-29 11:22:43 +0800

    update UVa12019 - Doom's Day Algorithm

--- UVa/V120/12019 - Doom's Day Algorithm.cpp
+++ UVa/V120/12019 - Doom's Day Algorithm.cpp
@@ -1,22 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int T, m, d, now;
-int table[12 + 5][31 + 10], day[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-string name[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+const string s[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+int day[] = {4, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main() {
-    now = 4;
-    for (int i = 1; i <= 12; i++) {
-        for (int j = 1; j <= day[i - 1]; j++) {
-            now = (now + 1) % 7;
-            table[i][j] = now;
-        }
-    }
-
-    cin >> T;
-    while (T--) {
-        cin >> m >> d;
-        cout << name[table[m][d]] << '\n';
-    }
+	for (int i = 1; i < 12; i++) {
+		day[i] += day[i - 1];
+	}
+	int T, m, d;
+	cin >> T;
+	while (T--) {
+		cin >> m >> d;
+		cout << s[(day[m - 1] + d) % 7] << '\n';
+	}
+	return 0;
 }
