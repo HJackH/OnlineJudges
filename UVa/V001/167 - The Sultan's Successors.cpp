fa052d28d96b1fd13ba69f6362f2f9452c2cc66d
HJackH <fandy36@gmail.com>
2021-11-10 04:39:21 +0800

    update UVa167 - The Sultan's Successors

--- UVa/V001/167 - The Sultan's Successors.cpp
+++ UVa/V001/167 - The Sultan's Successors.cpp
@@ -3,23 +3,18 @@ using namespace std;
 
 const int MAXN = 8;
 
-int ans, place[MAXN + 5], board[MAXN + 5][MAXN + 5];
+int ans, board[MAXN + 5][MAXN + 5];
 bool vis[3][2 * MAXN + 5];
 
-void dfs(int n) {
+void dfs(int n, int sum) {
     if (n == 9) {
-        int sum = 0;
-        for (int i = 1; i <= MAXN; i++) {
-            sum += board[place[i]][i];
-        }
         ans = max(ans, sum);
         return;
     }
     for (int i = 1; i <= MAXN; i++) {
         if (!vis[0][i] && !vis[1][n + i] && !vis[2][n - i + MAXN]) {
             vis[0][i] = vis[1][n + i] = vis[2][n - i + MAXN] = true;
-            place[n] = i;
-            dfs(n + 1);
+            dfs(n + 1, sum + board[n][i]);
             vis[0][i] = vis[1][n + i] = vis[2][n - i + MAXN] = false;
         }
     }
@@ -37,7 +32,7 @@ int main() {
 
         ans = 0;
         memset(vis, 0, sizeof(vis));
-        dfs(1);
+        dfs(1, 0);
         printf("%5d\n", ans);
     }   
 }
