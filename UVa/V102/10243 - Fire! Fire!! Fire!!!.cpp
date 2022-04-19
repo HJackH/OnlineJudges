53bba9480f07ba99a6275b190ac14168a84bf922
HJackH <fandy36@gmail.com>
2022-04-19 16:00:30 +0800

    update UVa10243 - Fire! Fire!! Fire!!!

--- UVa/V102/10243 - Fire! Fire!! Fire!!!.cpp
+++ UVa/V102/10243 - Fire! Fire!! Fire!!!.cpp
@@ -1,47 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-using LL = long long;
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);
 #define pb push_back
-const int MAXN = 1e4 + 5;
+const int MAXN = 1000 + 5;
 
 int n, dp[MAXN][2];
-bool vis[MAXN];
 vector<int> G[MAXN];
 
 void dfs(int u, int from) {
-    vis[u] = 1;
     dp[u][0] = 0;
     dp[u][1] = 1;
     for (int v : G[u]) {
-        if (!vis[v] && v != from) {
-            dfs(v, u);
-            dp[u][0] += dp[v][1];
-            dp[u][1] += min(dp[v][0], dp[v][1]);
+        if (v == from) {
+            continue;
         }
+        dfs(v, u);
+        dp[u][1] += min(dp[v][0], dp[v][1]);
+        dp[u][0] += dp[v][1];
     }
 }
 
 int main() { IOS
     while (cin >> n && n) {
         for (int i = 1; i <= n; i++) {
-            vis[i] = 0;
             G[i].clear();
-            int m, to;
-            cin >> m;
-            for (int j = 0; j < m; j++) {
-                cin >> to;
-                G[i].pb(to);
-            }
-        }   
-        
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (!vis[i]) {
-                dfs(i, -1);
-                ans += max(min(dp[i][0], dp[i][1]), 1);
+        }
+        for (int i = 1, k, x; i <= n; i++) {
+            cin >> k;
+            while (k--) {
+                cin >> x;
+                G[i].pb(x);
             }
         }
-        cout << ans << '\n';
+        dfs(1, -1);
+        cout << max(1, min(dp[1][0], dp[1][1])) << "\n";
     }
 }
