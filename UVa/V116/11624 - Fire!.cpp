31b5baeef53ae9c2d5b0b253e3c6220773b57b5a
HJackH <fandy36@gmail.com>
2022-06-22 03:01:04 +0800

    update UVa11624 - Fire!

--- UVa/V116/11624 - Fire!.cpp
+++ UVa/V116/11624 - Fire!.cpp
@@ -1,100 +1,124 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int MAXN = 1000 + 5;
 const int INF = 1e9;
-const int way[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-struct Node{
-    int x;
-    int y;
-    int depth;
+const int MAXN = 1000 + 5;
+const int way[4][2] = {
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0}
 };
 
-int r, c, fireOn[MAXN + 5][MAXN + 5], vis[MAXN + 5][MAXN + 5];
-string lines[MAXN + 5];
+struct Pos {
+    int x;
+    int y;
+    int w;
+};
 
+int n, m;
+string board[MAXN];
+int vis[MAXN][MAXN];
 
-void bfsOnFire(Node st) {
-    queue<Node> q;
-    q.push(st);
-    fireOn[st.x][st.y] = -1;
+bool in(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+bool edge(int x, int y) {
+    return x == 0 || x == n - 1 || y == 0 || y == m - 1;
+}
+
+void bfs_fire(int stx, int sty) {
+    vis[stx][sty] = 0;
+    queue<Pos> q;
+    q.push({stx, sty, 0});
     while (!q.empty()) {
         auto now = q.front();
         q.pop();
+        if (now.w > vis[now.x][now.y]) {
+            continue;
+        }
         for (int i = 0; i < 4; i++) {
             int tx = now.x + way[i][0];
             int ty = now.y + way[i][1];
-            if (tx >= 0 && tx < r && ty >= 0 && ty < c 
-                    && lines[tx][ty] != '#' && fireOn[tx][ty] > (now.depth + 1)) {
-                fireOn[tx][ty] = now.depth + 1;
-                q.push({tx, ty, now.depth + 1});
+            if (!in(tx, ty)) {
+                continue;
+            }
+            if (board[tx][ty] == '#') {
+                continue;
+            }
+            if (vis[tx][ty] > now.w + 1) {
+                vis[tx][ty] = now.w + 1;
+                q.push({tx, ty, vis[tx][ty]});
             }
         }
     }
 }
 
-void bfsOnJoe(Node st) {
-    memset(vis, 0, sizeof(vis));
-
-    queue<Node> q;
-    q.push(st);
-    vis[st.x][st.y] = true;
+int bfs_joe(int stx, int sty) {
+    vis[stx][sty] = 0;
+    queue<Pos> q;
+    q.push({stx, sty, 0});
     while (!q.empty()) {
         auto now = q.front();
         q.pop();
-        if ((now.x == 0 || now.x == r - 1) || (now.y == 0 || now.y == c - 1)) {
-            cout << now.depth + 1 << '\n';
-            return;
+        if (edge(now.x, now.y)) {
+            return now.w;
         }
-
+        if (now.w > vis[now.x][now.y]) {
+            continue;
+        }
         for (int i = 0; i < 4; i++) {
             int tx = now.x + way[i][0];
             int ty = now.y + way[i][1];
-            if (tx >= 0 && tx < r && ty >= 0 && ty < c
-                    && lines[tx][ty] != '#' && fireOn[tx][ty] > (now.depth + 1) && !vis[tx][ty]) {
-                vis[tx][ty] = true;
-                q.push({tx, ty, now.depth + 1});
+            if (!in(tx, ty)) {
+                continue;
+            }
+            if (board[tx][ty] == '#') {
+                continue;
+            }
+            if (vis[tx][ty] > now.w + 1) {
+                vis[tx][ty] = now.w + 1;
+                q.push({tx, ty, vis[tx][ty]});
             }
         }
     }
-    cout << "IMPOSSIBLE\n";
+    return -1;
+}
+
+void sol() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> board[i];
+        for (int j = 0; j < m; j++) {
+            vis[i][j] = INF;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 'F') {
+                bfs_fire(i, j);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 'J') {
+                int ans = bfs_joe(i, j);
+                if (ans == -1) {
+                    cout << "IMPOSSIBLE\n";
+                } else {
+                    cout << ans + 1 << "\n";
+                }
+                return;
+            }
+        }
+    }
 }
 
 int main() {
     int T;
     cin >> T;
     while (T--) {
-        cin >> r >> c;
-
-        Node joe;
-        vector<Node> fires;
-        bool findj = false, findf = false;
-        for (int i = 0; i < r; i++) {
-            cin >> lines[i];
-            for (int j = 0, ed = lines[i].length(); j < ed; j++) {
-                if (!findj && lines[i][j] == 'J') {
-                    findj = true;
-                    joe = {i, j, 0};
-                } 
-                if (lines[i][j] == 'F') {
-                    findf = true;
-                    fires.push_back({i, j, 0});
-                }
-            }
-        }
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                fireOn[i][j] = INF;
-            }
-        }
-
-        if (findf) {
-            for (auto fire : fires) {
-                bfsOnFire(fire);
-            }
-        }
-        bfsOnJoe(joe);
+        sol();
     }
 }
